0b05e6dff3cf3d18b96f571232c567fd98065e09
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 10:44:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import io
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -9,7 +8,7 @@ st.title("Gender Prediction Based on Vietnamese full Names ")
 #image = Image.open("vinames.png")
 #st.image(image)
 
-nput_model = open('lrc_admit.pkl', 'rb')
+nput_model = open('lr_admit.pkl', 'rb')
 model = pkl.load(input_model)
 
 st.header('Input addmission information')
