babe9ae51ec3dfcf8135e8147db4aaab0eb80561
Nguyen Phu Hao <126962580+Junkohoutarou@users.noreply.github.com>
2023-12-08 10:47:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+from PIL import Image
+import pickle as pkl
+import numpy as np
 
-st.title("Gender Prediction Based on Vietnamese full Names ")
+st.title('USA college admission rate prediction')
 
-#image = Image.open("vinames.png")
+#image = Image.open('college admission.jpg')
 #st.image(image)
 
-nput_model = open('lr_admit.pkl', 'rb')
-model = pkl.load(input_model)
+input = open('lr_admit.pkl', 'rb')
+model = pkl.load(input)
 
-st.header('Input addmission information')
+st.header('Input admission information')
 gre = st.number_input('Insert GRE Score')
 toefl = st.number_input('Insert TOEFL Score')
 uni_rate = st.number_input('Insert University Rating')
 sop = st.number_input('Insert SOP')
 lor = st.number_input('Insert LOR')
 cgpa = st.number_input('Insert CGPA')
-research =st.radio('Choose Research',[0,1], index=None')
+research = st.radio('Choose Research', [0, 1], index=None)
+
+if gre is not None and toefl is not None and uni_rate is not None and sop is not None and lor is not None and cgpa is not None and research is not None:
+    if st.button('Predict'):
+        feature_vector = np.array([gre, toefl, uni_rate, sop, lor, cgpa, research]).reshape(1,-1)
+        result = str((model.predict(feature_vector)[0])[0])
+
+        st.header('Result')
+        st.text(result)
